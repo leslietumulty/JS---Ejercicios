a2914580d6424e4e390bbbc4de9a139fbb3543dc
leslie <leslietumulty@hotmail.com>
2019-07-06 04:00:58 -0300

    ejercicios JavaScript

---
+++ ejercicio.js
@@ -0,0 +1,11 @@
+var nombre;
+var apellido;
+var edad;
+var fechaDeNacimiento;
+var direccion;
+
+nombre = 'leslie';
+apellido = 'montoya';
+edad = 35;
+fechaDeNacimiento = '30/01/1984';
+direccion = 'benito juarez 3361, devoto';
